82928ccea24d79e2a055c7c7545e87e35ad0a5a3
BingWiki <stbtl@163.com>
2020-03-24 10:58:22 +0800

    create

---
+++ src/main/java/com/yxb/dd/DDRefactoringApplication.java
@@ -0,0 +1,12 @@
+package com.yxb.dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DDRefactoringApplication.class, args);
+    }
+}
