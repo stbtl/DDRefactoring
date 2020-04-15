0685f4376a8cca78bdfd2c17bc6b85d4ee09f0d0
BingWiki <stbtl@163.com>
2020-04-15 16:56:54 +0800

    i18n

---
+++ src/main/java/com/yxb/dd/config/WebMvcConfig.java
@@ -0,0 +1,12 @@
+package com.yxb.dd.config;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author YingXB
+ * @date 2020/04/14 15:44
+ */
+public class WebMvcConfig implements WebMvcConfigurer {
+
+}
