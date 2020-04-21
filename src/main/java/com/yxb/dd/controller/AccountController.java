16783958c15c2535b20290f011ce3434cdb0c42c
BingWiki <stbtl@163.com>
2020-04-21 09:40:21 +0800

    页面模块化 fragment insert

--- src/main/java/com/yxb/dd/controller/AccountController.java
+++ src/main/java/com/yxb/dd/controller/AccountController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.math.BigInteger;
 
@@ -42,6 +41,6 @@ public class AccountController {
     public String userDetain(@PathVariable BigInteger id, @PathVariable String operateType, Model model) {
         model.addAttribute("user", accountService.getUserById(id));
         model.addAttribute("operateType", operateType);
-        return "user_detain";
+        return "user_detail";
     }
 }
